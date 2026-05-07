4c4704ca557fe6512584f14bbdcfdb28f67d1a83
Andrey Bilenko <olimp_72@ukr.net>
2026-05-08 00:00:55 +0300

    Solution

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # Підключаємо URL-адреси додатка cinema з правильним префіксом
+    path("api/cinema/", include("cinema.urls", namespace="cinema")),
 ]
